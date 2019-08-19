58d1221d00adf0fcee396b64009934a769f18b31
austin loveless <austinloveless5171@gmail.com>
2019-08-19 12:18:17 -0600

    packages

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const port = 3000 || process.env.PORT;
 
+app.use(cors()); 
+app.use(bodyParser.json()); 
+
 app.get('/', (req, res) => {
     res.send('NodeServer'); 
 }); 
